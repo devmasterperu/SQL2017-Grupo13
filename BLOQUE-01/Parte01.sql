35063fc8a3895d3ffdbb6142ce21a29ab90df22b
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-06-03 22:00:36 -0500

    Bloque 01 Parte 01

---
+++ BLOQUE-01/Parte01.sql
@@ -0,0 +1,14 @@
+--BLOQUE 02
+/*VARIABLES*/
+--declare @num1 int=20
+--declare @num2 int=3
+
+--select @num1+@num2 as suma, @num1-@num2 as resta,@num1*@num2 as multiplica,@num1/@num2 as division,@num1%@num2 as modulo
+
+--declare @num1 varchar(20)=20
+--declare @num2 varchar(20)=3
+
+declare @num1 varchar(20)='CURSO'
+declare @num2 varchar(20)=' BASE DE DATOS'
+
+select @num1+@num2 as suma
