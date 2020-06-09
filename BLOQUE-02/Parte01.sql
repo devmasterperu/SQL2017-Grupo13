592d0c37577dc42fdc33639590cf4bc0edce2463
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-06-08 20:25:03 -0500

    Archivos bloque 02

--- BLOQUE-02/Parte01.sql
+++ BLOQUE-02/Parte01.sql
@@ -17,3 +17,12 @@ declare @_r bigint=2
 --select power(2,3),power(3,4)--power(base,potencia)
 
 select @_t1*power(@_r,@_n-1) as TN
+
+--Lógica de procesamiento
+
+select codubigeo,count(codzona) as total --(V)
+from Zona --(I)
+where estado=1 --(II)
+group by codubigeo --(III)
+having count(codzona)>=4 --(IV)
+order by codubigeo desc --(VI)
