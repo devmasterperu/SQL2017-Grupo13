f3a11bc8c673a45eb2faf55b4e9557eb7c91c473
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-06-17 20:20:25 -0500

    Archivos Bloque 03 B

---
+++ BLOQUE-03/CargaTelefono02.sql
@@ -0,0 +1,24 @@
+--Carga Telefono-2
+--select * from Cliente where codcliente=1001
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380328',1001,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380329',1002,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380330',1003,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380331',1004,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380332',1005,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380333',1006,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380334',1007,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380335',1008,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380336',1009,1)
+go
+insert into Telefono(tipo,numero,codcliente,estado) values ('LLA ','012380337',1010,0)
+go
+
+select * from Telefono
