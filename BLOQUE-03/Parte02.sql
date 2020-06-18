30491dbdb97b13823a35ef5f6e221d4f2ae39170
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-06-17 20:01:40 -0500

    Archivos Bloque 03 B

--- BLOQUE-03/Parte02.sql
+++ BLOQUE-03/Parte02.sql
@@ -8,3 +8,19 @@ inner join Zona as z on c.codzona=z.codzona
 where t.estado=1 and c.tipo_cliente='E'--c.estado=1
 order by c.codcliente asc
 
+--03.07
+
+select  tipo as TIPO, numero as NUMERO, concat (c.nombres,' ',c.ape_paterno,'',c.ape_materno) as NOMBRES, c.email as EMAIL, 
+z.nombre as ZONA,
+concat( u.cod_dpto,u.cod_prov,u.cod_dto) as UBIGEO  
+from telefono  t
+inner join cliente c on t.codcliente=c.codcliente
+inner join zona z on c.codzona=z.codzona --codzona VS codzona
+inner join ubigeo u on z.codubigeo=u.codubigeo
+where t.estado=0 and c.tipo_cliente='P'
+order by c.email desc
+
+
+
+
+
