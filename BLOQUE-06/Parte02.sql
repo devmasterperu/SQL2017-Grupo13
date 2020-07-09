7fd7b47705fc92b43e085b4280b020afe5bb240a
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-07-08 21:13:10 -0500

    Parte 02 Bloque 06

--- BLOQUE-06/Parte02.sql
+++ BLOQUE-06/Parte02.sql
@@ -46,3 +46,93 @@ ntile(4) over(partition by c.codzona order by rt.total asc) as R4
 from Cliente c left join CTE_RT rt on c.codcliente=rt.codcliente
 where tipo_cliente='P'
 order by ZONA asc, N_TEL asc
+
+--06.07
+
+--TABLAS_DERIVADAS
+select c.codcliente as #,razon_social as CLIENTE, codzona as ZONA,isnull(rc.total,0) as TOTAL,
+/*Es la posición irrepetible por zona. Considerar el total de contratos, de menor a mayor, 
+para generar la posición*/
+row_number() over (partition by codzona order by rc.total asc) as E1,
+/*Es el valor previo de la razón_social evaluada, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+lag(razon_social) over (partition by codzona order by rc.total asc) as E2,
+/*Es el valor siguiente de la razón_social evaluada, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+lead(razon_social) over (partition by codzona order by rc.total asc) as E3,
+/*Es el primer valor de la razón_social hasta el momento de la evaluación, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+first_value(razon_social) over (partition by codzona order by rc.total asc) as E3,
+/*Es el último valor de la razón_social hasta el momento de la evaluación, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+last_value(razon_social) over (partition by codzona order by rc.total asc) as E4
+from Cliente c
+left join
+(
+  select codcliente,count(codplan) as total 
+  from Contrato
+  group by codcliente
+) rc on c.codcliente=rc.codcliente
+where tipo_cliente='E'
+order by ZONA asc, rc.total asc
+
+--CTES
+WITH CTE_RC AS
+(
+  select codcliente,count(codplan) as total 
+  from Contrato
+  group by codcliente
+)
+select c.codcliente as #,razon_social as CLIENTE, codzona as ZONA,isnull(rc.total,0) as TOTAL,
+/*Es la posición irrepetible por zona. Considerar el total de contratos, de menor a mayor, 
+para generar la posición*/
+row_number() over (partition by codzona order by rc.total asc) as E1,
+/*Es el valor previo de la razón_social evaluada, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+lag(razon_social,1,'SIN DATO') over (partition by codzona order by rc.total asc) as E2,
+/*Es el valor siguiente de la razón_social evaluada, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+lead(razon_social,1,'SIN DATO') over (partition by codzona order by rc.total asc) as E3,
+/*Es el primer valor de la razón_social hasta el momento de la evaluación, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+isnull(first_value(razon_social) over (partition by codzona order by rc.total asc),'SIN DATO') as E3,
+/*Es el último valor de la razón_social hasta el momento de la evaluación, considerando el total de contratos (Menor a mayor) 
+dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+isnull(last_value(razon_social) over (partition by codzona order by rc.total asc),'SIN DATO') as E4
+from Cliente c left join CTE_RC rc 
+on c.codcliente=rc.codcliente
+where tipo_cliente='E'
+order by ZONA asc, rc.total asc
+
+--FUNCION_VALOR_TABLA
+
+create function F_RESUMEN_CLIENTE() returns table
+as return
+	WITH CTE_RC AS
+	(
+	  select codcliente,count(codplan) as total 
+	  from Contrato
+	  group by codcliente
+	)
+	select c.codcliente as #,razon_social as CLIENTE, codzona as ZONA,isnull(rc.total,0) as TOTAL,
+	/*Es la posición irrepetible por zona. Considerar el total de contratos, de menor a mayor, 
+	para generar la posición*/
+	row_number() over (partition by codzona order by rc.total asc) as E1,
+	/*Es el valor previo de la razón_social evaluada, considerando el total de contratos (Menor a mayor) 
+	dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+	lag(razon_social,1,'SIN DATO') over (partition by codzona order by rc.total asc) as E2,
+	/*Es el valor siguiente de la razón_social evaluada, considerando el total de contratos (Menor a mayor) 
+	dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+	lead(razon_social,1,'SIN DATO') over (partition by codzona order by rc.total asc) as E3,
+	/*Es el primer valor de la razón_social hasta el momento de la evaluación, considerando el total de contratos (Menor a mayor) 
+	dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+	isnull(first_value(razon_social) over (partition by codzona order by rc.total asc),'SIN DATO') as E4,
+	/*Es el último valor de la razón_social hasta el momento de la evaluación, considerando el total de contratos (Menor a mayor) 
+	dentro de cada zona. Mostrar ‘SIN DATO’ para valores desconocidos*/
+	isnull(last_value(razon_social) over (partition by codzona order by rc.total asc),'SIN DATO') as E5
+	from Cliente c left join CTE_RC rc 
+	on c.codcliente=rc.codcliente
+	where tipo_cliente='E'
+
+	select * from F_RESUMEN_CLIENTE()
+	order by ZONA asc, TOTAL asc
