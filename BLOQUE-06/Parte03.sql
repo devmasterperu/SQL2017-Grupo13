310af6c5c85a716e2c74f38cd95d0ad7cdacb5ce
Gianfranco Manrique <gmanrique.422@gmail.com>
2020-07-13 18:36:48 -0500

    Archivos Bloque 06 parte 03

--- BLOQUE-06/Parte03.sql
+++ BLOQUE-06/Parte03.sql
@@ -37,3 +37,44 @@ ordenado por número de menor a mayor*/
 row_number() over (partition by it.codcliente order by it.numero asc) as POSICION
 from CTE_IT it left join Cliente c on it.codcliente=c.codcliente
 order by it.codcliente,it.numero
+
+--b
+
+--Los teléfonos que se encuentran en el esquema ‘dbo’ pero no en ‘comercial’
+select tipo,numero,codcliente,estado from dbo.Telefono --1203
+except 
+select tipo,numero,codcliente,estado  from comercial.Telefono --50
+
+WITH CTE_ET AS
+(
+--Los teléfonos que se encuentran en el esquema ‘comercial’ pero no en ‘dbo’
+select tipo,numero,codcliente,estado from comercial.Telefono --1203
+except 
+select tipo,numero,codcliente,estado  from dbo.Telefono --50
+)
+select et.codcliente as CODIGO,
+case 
+	when c.tipo_cliente='E' then c.razon_social 
+	when c.tipo_cliente='P' then c.nombres+' '+c.ape_paterno+' '+c.ape_materno
+	else 'SIN DATO'
+end as CLIENTE,et.tipo as TIPO,et.numero as NUMERO,et.estado as ESTADO,
+/*Es la posición que ocupa el teléfono dentro de cada cliente, 
+ordenado por número de menor a mayor*/
+row_number() over (partition by et.codcliente order by et.numero asc) as POSICION
+from CTE_ET as et left join Cliente c on et.codcliente=c.codcliente
+order by et.codcliente,et.numero asc
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
